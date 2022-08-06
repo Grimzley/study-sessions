d1e8d638efaa676442194983a2909b89bd2a393c
Michael Grimsley <mgrimsley8460@sdsu.edu>
2022-08-06 15:45:15 -0700

    added service worker install, activate, and fetch events

--- sw.js
+++ sw.js
@@ -1,1 +1,12 @@
-
+// Install Event
+self.addEventListener('install', (evt) => {
+    console.log('service worker installed');
+});
+// Activate Event
+self.addEventListener('activate', (evt) => {
+    console.log('service worker activated');
+});
+// Fetch Event
+self.addEventListener('fetch', (evt) => {
+    console.log('fetch event: ', evt);
+});
