743bcc059d2866e407ab863e5f708e977c0161fc
Michael Grimsley <mgrimsley8460@sdsu.edu>
2022-08-09 02:30:31 -0700

    updated UI to reflect database information

--- js/db.js
+++ js/db.js
@@ -3,6 +3,7 @@ db.collection('events').onSnapshot((snapshot) => {
     snapshot.docChanges().forEach((change) => {
         if (change.type === 'added') {
             // add data
+            renderEvent(change.doc.data(), change.doc.id);
         }else if (change.type === 'removed') {
             // remove data
         }
--- js/ui.js
+++ js/ui.js
@@ -1,3 +1,5 @@
+const events = document.querySelector('.events');
+
 document.addEventListener('DOMContentLoaded', function() {
     // Menu
     const menu = document.querySelectorAll('.side-menu');
@@ -6,3 +8,19 @@ document.addEventListener('DOMContentLoaded', function() {
     const form = document.querySelectorAll('.side-form');
     M.Sidenav.init(form, {edge: 'left'});
 });
+// Render Event Data
+const renderEvent = (data, id) => {
+    const html = `
+        <div class="card-panel content white row" data-id="${id}">
+            <img src="/img/content.png" alt="content img">
+            <div class="content-details">
+                <div class="content-title">${data.title}</div>
+                <div class="content-info">${data.info}</div>
+            </div>  
+            <div class="content-delete">
+                <i class="material-icons" data-id="${id}">delete_outline</i>
+            </div>
+        </div>
+    `;
+    events.innerHTML += html;
+};
